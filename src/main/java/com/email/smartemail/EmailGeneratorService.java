efacd0c0f5974cdf51632b4acd775acdff6da755
Raj Jaiswal <rajjaiswal6394@gmail.com>
2026-06-28 10:03:13 +0530

    first commit

---
+++ src/main/java/com/email/smartemail/EmailGeneratorService.java
@@ -0,0 +1,4 @@
+package com.email.smartemail;
+
+public class EmailgeneratorService {
+}
---
+++ src/test/java/com/email/smartemail/SmartEmailAssistantApplicationTests.java
@@ -0,0 +1,10 @@
+package com.email.smartemail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartEmailAssistantApplicationTests {
+
+    @Test
+    void contextLoads() {
